7bcebfedd231ac30aa419b09ca173aa97c7d4ad2
Bimendra Bandara <bimendra.85@gmail.com>
2017-07-07 20:59:21 +0530

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express();
 
+app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-    console.log('Server running on http://localhost:3000');
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log('Server running on http://localhost:' + port);
 })
